13d9469a7bef8be1cddf2315da821957313e16dc
VinoSheesh <orvinoray@gmail.com>
2025-01-08 11:23:29 +0700

    Menambahkan Icon,dan cardview

--- app/src/main/java/com/tugasss/laundryapp/HomeActivity.kt
+++ app/src/main/java/com/tugasss/laundryapp/HomeActivity.kt
@@ -5,12 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+
+        val greetingTextView: TextView = findViewById(R.id.greetingTextView)
+        val dateTextView: TextView = findViewById(R.id.dateTextView)
+
+        val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+        val greeting = when (currentHour) {
+            in 0..11 -> "Selamat Pagi,Vino"
+            in 12..15 -> "Selamat Siang,Vino"
+            in 16..18 -> "Selamat Sore,Vino"
+            else -> "Selamat Malam,Vino"
+        }
+
+        greetingTextView.text = greeting
+
+        val dateFormat = SimpleDateFormat("EEEE, dd MMMM yyyy", Locale.getDefault())
+        val currentDate = dateFormat.format(Date())
+        dateTextView.text = currentDate
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
